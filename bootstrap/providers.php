f38a21c585343bbf48953079fa85829827908793
rajabrajabb <141490342+rajabrajab@users.noreply.github.com>
2025-10-20 16:30:12 +0300

    remove telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\ResponseServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
