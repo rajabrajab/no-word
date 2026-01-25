279d3f58d4b0cacbc74fd67bbb8d0c0bbce4463f
rajabrajabb <141490342+rajabrajab@users.noreply.github.com>
2026-01-25 15:04:23 +0300

    add mail blade

---
+++ resources/views/otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP Code</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>Your OTP code is: <strong>{{ $otp }}</strong></p>
+    <p>This code is valid for 15 minutes.</p>
+    <p>If you did not request this, please ignore this email.</p>
+</body>
+</html>
